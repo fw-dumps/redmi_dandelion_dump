2f7b811c1ea6242491a4b90c572ef473d3bdd828
atharvnegi <atharvnegi2951@gmail.com>
2022-03-18 09:31:25 +0000

    Add extras for dandelion-user 11 RP1A.200720.011 V12.5.3.0.RCDMIXM release-keys
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## dandelion-user 11 RP1A.200720.011 V12.5.3.0.RCDMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6765
+- Codename: dandelion
+- Brand: Redmi
+- Flavor: dandelion-user
+- Release Version: 11
+- Id: RP1A.200720.011
+- Incremental: V12.5.3.0.RCDMIXM
+- Tags: release-keys
+- CPU Abilist: armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Redmi/dandelion_global/dandelion:11/RP1A.200720.011/V12.5.3.0.RCDMIXM:user/release-keys
+- OTA version: 
+- Branch: dandelion-user-11-RP1A.200720.011-V12.5.3.0.RCDMIXM-release-keys
+- Repo: redmi_dandelion_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk
+rm -f system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null
